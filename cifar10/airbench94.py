f6905f7f1449ad4aa2e6961c58df3ed73936fc70
Yash Samat <ysamat@seas.upenn.edu>
2025-12-10 14:32:00 -0500

    cifar10 code change for experiment 1

--- cifar10/airbench94.py
+++ cifar10/airbench94.py
@@ -38,7 +38,7 @@ torch.backends.cudnn.benchmark = True
 
 hyp = {
     'opt': {
-        'train_epochs': 9.9,
+        'train_epochs': 6.0,
         'batch_size': 1024,
         'lr': 11.5,                 # learning rate per 1024 examples
         'momentum': 0.85,
