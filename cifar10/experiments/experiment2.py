8a4b22d1d56bd8f3890ce4bee71e8a1ca35cbf6d
Yash Samat <ysamat@seas.upenn.edu>
2025-12-10 16:25:05 -0500

    change eval

--- cifar10/experiments/experiment2.py
+++ cifar10/experiments/experiment2.py
@@ -309,32 +309,29 @@ def print_training_details(variables, is_final_entry):
 
 def infer(model, loader, tta_level=0):
 
-    # Test-time augmentation strategy (for tta_level=2):
-    # 1. Flip/mirror the image left-to-right (50% of the time).
-    # 2. Translate the image by one pixel either up-and-left or down-and-right (50% of the time,
-    #    i.e. both happen 25% of the time).
-    #
-    # This creates 6 views per image (left/right times the two translations and no-translation),
-    # which we evaluate and then weight according to the given probabilities.
-
     def infer_basic(inputs, net):
-        return net(inputs).clone()
+        with torch.cuda.amp.autocast():
+            return net(inputs).clone()
 
     def infer_mirror(inputs, net):
-        return 0.5 * net(inputs) + 0.5 * net(inputs.flip(-1))
+        with torch.cuda.amp.autocast():
+            return 0.5 * net(inputs) + 0.5 * net(inputs.flip(-1))
 
     def infer_mirror_translate(inputs, net):
-        logits = infer_mirror(inputs, net)
-        pad = 1
-        padded_inputs = F.pad(inputs, (pad,)*4, 'reflect')
-        inputs_translate_list = [
-            padded_inputs[:, :, 0:32, 0:32],
-            padded_inputs[:, :, 2:34, 2:34],
-        ]
-        logits_translate_list = [infer_mirror(inputs_translate, net)
-                                 for inputs_translate in inputs_translate_list]
-        logits_translate = torch.stack(logits_translate_list).mean(0)
-        return 0.5 * logits + 0.5 * logits_translate
+        with torch.cuda.amp.autocast():
+            logits = infer_mirror(inputs, net)
+            pad = 1
+            padded_inputs = F.pad(inputs, (pad,)*4, 'reflect')
+            inputs_translate_list = [
+                padded_inputs[:, :, 0:32, 0:32],
+                padded_inputs[:, :, 2:34, 2:34],
+            ]
+            logits_translate_list = [
+                infer_mirror(inputs_translate, net)
+                for inputs_translate in inputs_translate_list
+            ]
+            logits_translate = torch.stack(logits_translate_list).mean(0)
+            return 0.5 * logits + 0.5 * logits_translate
 
     model.eval()
     test_images = loader.normalize(loader.images)
